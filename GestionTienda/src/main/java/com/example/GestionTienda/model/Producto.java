b3df46a1b26ee50fbbe1618a44e5f8844e8f9b96
mgaangel10 <mg.aangel10@gmail.com>
2024-01-28 20:51:28 +0100

    atributes de productos

--- GestionTienda/src/main/java/com/example/GestionTienda/model/Producto.java
+++ GestionTienda/src/main/java/com/example/GestionTienda/model/Producto.java
@@ -1,5 +1,30 @@
 package com.example.GestionTienda.model;
 
-public class Producto {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Producto extends BaseEntity{
+
+
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private String imagen;
+    private boolean disponible;
+    @Transient
+    private Integer productoCategoriaId;
     
 }
