1fb5fad9cdcf70f091d1d81850ccd83f1f3fa2e4
mgaangel10 <mg.aangel10@gmail.com>
2024-01-28 20:06:22 +0100

    second commit

---
+++ GestionTienda/src/test/java/com/example/GestionTienda/GestionTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionTienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
