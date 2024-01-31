127296b856f7f2753f467b431a45aaf9c78098c3
ALVARO RUIZ <alvruigut@alum.us.es>
2024-01-31 01:33:50 +0100

    cors aprendido, mañana lo mejoro

--- GestionTienda/frontend/src/App.js
+++ GestionTienda/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes,Link}from 'react-router-dom';
 import { Fragment } from 'react';
-
+import { useState, useEffect } from 'react';
 function HomePage() {
   return (
     <Fragment>
@@ -18,16 +18,41 @@ function HomePage() {
 }
 
 function AllProducts() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:9000/api/productos/all');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-      <div className='App-header'>
-        <h1>Todos Los Productos</h1>
-        <ul>
-          <li> 
-            <Link to="/">Pantalla Inicial</Link>
-          </li>
-        </ul>
-      </div>
-  )}
+    <div className='App-header'>
+      <h1>Todos Los Productos</h1>
+      <ul>
+        <li> 
+          <Link to="/">Pantalla Inicial</Link>
+        </li>
+        {products.map(product => (
+          <li key={product.id}>{product.nombre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
   return (
