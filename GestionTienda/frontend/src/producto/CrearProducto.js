c2e6e92bdf7ab052268e72754d90bbb2680bf1f2
ALVARO RUIZ <alvruigut@alum.us.es>
2024-02-03 02:03:20 +0100

    cositas ya

--- GestionTienda/frontend/src/producto/CrearProducto.js
+++ GestionTienda/frontend/src/producto/CrearProducto.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
+import Gallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 export function CrearProducto() {
   const navigate = useNavigate();
   const [producto, setProducto] = useState({
-    imagen: '',
+    imagenes: [],
     nombre: '',
     descripcion: '',
     precio: 0,
@@ -22,7 +25,14 @@ export function CrearProducto() {
     });
   };
 
-  const handleSubmit = async (e) => {
+  const handleDrop = (acceptedFiles) => {
+    setProducto({
+      ...producto,
+      imagenes: producto.imagenes.concat(acceptedFiles.map(file => ({ file, src: URL.createObjectURL(file) })))
+    });
+  };
+
+ const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!producto.imagen) {
@@ -48,23 +58,29 @@ export function CrearProducto() {
     }
   };
 
+  const { getRootProps, getInputProps } = useDropzone({ onDrop: handleDrop, accept: 'image/*' });
+
   return (
     <div style={styles.container}>
       <form onSubmit={handleSubmit} style={styles.form}>
-        <input type="text" name="imagen" onChange={handleChange} placeholder="URL de la imagen" style={styles.input} />
+      <div {...getRootProps()} style={styles.dropzone}>
+          <input {...getInputProps()} />
+          <p>Arrastra imágenes aquí o haz clic para seleccionar</p>
+        </div >
+        <Gallery  style={styles.galleryContainer}  items={producto.imagenes} showPlayButton={false} showFullscreenButton={false} />
+  
         <input type="text" name="nombre" onChange={handleChange} placeholder="Nombre del producto" required style={styles.input} />
         <textarea name="descripcion" onChange={handleChange} placeholder="Descripción del producto" style={styles.textarea}></textarea>
         <input type="number" name="precio" onChange={handleChange} placeholder="Precio" required style={styles.input} />
-        <label style={styles.checkboxLabel}>
-          Disponible:
-          <input type="checkbox" name="disponible" onChange={e => setProducto({ ...producto, disponible: e.target.checked })} checked={producto.disponible} style={styles.checkbox} />
-        </label>
         <input type="number" name="categoria.id" onChange={handleChange} placeholder="ID de la categoría" required style={styles.input} />
         <button type="submit" style={styles.button}>Crear producto</button>
       </form>
     </div>
   );
 }
+
+
+
 
 const styles = {
   container: {
@@ -115,4 +131,15 @@ const styles = {
     cursor: 'pointer',
     transition: 'background-color 0.3s',
   },
+  dropzone: {
+    width: '90%',
+    minHeight: '10px',  // Ajusta la altura según tus necesidades
+    border: '2px dashed #ccc',
+    borderRadius: '4px',
+    padding: '20px',
+    textAlign: 'center',
+    cursor: 'pointer',
+    marginBottom: '20px',
+  },
+
 };
